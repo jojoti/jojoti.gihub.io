8c72487d453e849a33a1e0e1ab19fb957ec340a8
JoJo Wang <id233233@outlook.com>
2021-07-29 21:51:26 +0800

    feat: 时间线

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -104,7 +104,7 @@ module.exports = {
                     '/':
                         [
                             {
-                                title: '时间轴',
+                                title: 'Timeline',
                                 collapsable: false,
                                 children: [
                                     '',
